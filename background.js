3bdcf26601440de22b9a22d3bdc125b7436a6a4f
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-01-02 07:42:28 +0000

    added ui and js functionality

--- background.js
+++ background.js
@@ -1,4 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Phishing Detection Extension installed!");
-  });
-  
+// background.js
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "detectPhishing") {
+    const url = request.url;
+    fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ url })
+    })
+    .then(response => response.json())
+    .then(data => sendResponse(data))
+    .catch(error => {
+      console.error("Error:", error);
+      sendResponse({ error: "Unable to fetch prediction." });
+    });
+    return true; // Keep the messaging channel open for async response
+  }
+});
