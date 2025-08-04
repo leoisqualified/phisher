89e68aea865acab6249c1b2954326fb26b0ef7fa
leoisqualified <eaadenutsi@st.ug.edu.gh>
2025-08-04 11:00:26 +0000

    added company model to models.py

--- models.py
+++ models.py
@@ -6,13 +6,20 @@ db = SQLAlchemy()
 
 class URLLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    url = db.Column(db.String(2048))
     timestamp = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     prediction_score = db.Column(db.Float)
-    verdict = db.Column(db.String)
+    verdict = db.Column(db.String(32))
 
 class Blacklist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, nullable=False, unique=True)
-    reason = db.Column(db.String)
-    date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    url = db.Column(db.String(2048))
+    reason = db.Column(db.String(128))
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    api_key = db.Column(db.String(64), unique=True, nullable=False)  # For authentication
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
